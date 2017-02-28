c273a8cf0969f8a57e726e9189befdec61586061
Runagar <runagar@gmail.com>
2017-02-28 17:02:37 +0100

    added move script

---
+++ Assets/Scripts/Actions/Move.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Apex.AI;
+using UnityEngine;
+
+public sealed class Move : ActionWithOptions<Vector3>{
+
+    public override void Execute(IAIContext context)
+    {
+
+        var c = (PlayerContext)context;
+
+        throw new NotImplementedException();
+    }
+}
