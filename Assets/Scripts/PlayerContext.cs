39fe5037656b20c2eac80b78b589cf9bec8dd789
Bjacob93 <Zeint@live.dk>
2017-02-28 16:32:05 +0100

    PlayerContextStart

--- Assets/Scripts/PlayerContext.cs
+++ Assets/Scripts/PlayerContext.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Apex.AI;
 using UnityEngine;
 
-public class PlayerContext : MonoBehaviour {
+public class PlayerContext : IAIContext {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public PlayerContext(GameObject gameObject)
+    {
+        this.self = gameObject;
+        this.observations = new List<GameObject>();
+    }
+
+    public GameObject self
+    {
+        get;
+        private set;
+    }
+
+    public List<GameObject> observations
+    {
+        get;
+        private set;
+    }
+
 }
