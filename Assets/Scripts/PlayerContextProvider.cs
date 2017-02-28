6375501e82ed1009b657fc0b9813beaead56ba21
Bjacob93 <Zeint@live.dk>
2017-02-28 16:30:45 +0100

    contextProvider

---
+++ Assets/Scripts/PlayerContextProvider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Apex.AI;
+using Apex.AI.Components;
+
+public class PlayerContextProvider : MonoBehaviour, IContextProvider {
+
+    private IAIContext _context;
+
+    private void Awake()
+    {
+        _context = new PlayerContext(this.gameObject);
+    }
+
+    public IAIContext GetContext(Guid aiID)
+    {
+        return _context;
+    }
+
+}
