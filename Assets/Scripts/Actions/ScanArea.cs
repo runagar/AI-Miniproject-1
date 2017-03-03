f488e56dae7b093713d623a1bfe2adaa7523ad9c
Bjacob93 <Zeint@live.dk>
2017-03-03 14:24:37 +0100

    scan fix

--- Assets/Scripts/Actions/ScanArea.cs
+++ Assets/Scripts/Actions/ScanArea.cs
@@ -17,17 +17,20 @@ public sealed class ScanArea : ActionBase {
 
         var player =  c.self;
         c.observations.Clear();
-
+        Debug.Log("Scanning");
         //find stuff based on a overlapSphere
-        var colliders = Physics.OverlapSphere(player.transform.position, scanningRange, scanLayer);
-        
+        var colliders = Physics.OverlapSphere(player.transform.position, scanningRange);
+
+        Debug.Log("colliders length =  " + colliders.Length);
         for (int i = 0; i < colliders.Length; i++)
         {
             var col = colliders[i];
-
-            var furniture = col.GetComponent<GameObject>();
-
+            if (col.tag == "Furniture")
+            {
+                var furniture = col.gameObject;
+            Debug.Log("added  " + furniture.name);
             c.observations.Add(furniture);
+            }
         }
     }
 }
